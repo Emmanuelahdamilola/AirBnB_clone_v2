85f5620827927baeee2f09aebcce55875d72823b
Emmanuelahdamilola <emmanuelahdamilola1992@gmail.com>
2023-11-26 08:15:37 -0800

    Fabric script that generates a .tgz archive from the contents of the web_static folder

---
+++ 1-pack_web_static.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+# This is a Bash script for packaging a web application using Fabric.
+# It creates a compressed archive of the 'web_static' directory and stores it in the 'versions' directory.
+
+# Import necessary modules
+import os
+from datetime import datetime
+from fabric.api import local
+
+# Function to create a compressed archive of the 'web_static' directory
+def do_pack():
+    # Check if 'versions' directory exists; create it if not
+    if os.path.isdir("versions") is False:
+        # Attempt to create the 'versions' directory; return None if failed
+        if local("mkdir versions").failed is True:
+            return None
+
+    # Generate a unique file name for the compressed archive using the current date and time
+    dt = datetime.now()
+    file = f"versions/web-static_{dt.year}{dt.month}{dt.day}{dt.hour}{dt.minute}{dt.second}.tgz"
+
+    # Attempt to create a compressed archive; return None if failed
+    if local(f'tar -czvf {file} web_static').failed is True:
+        return None
+
+    # Return the path to the created compressed archive
+    return file
+
