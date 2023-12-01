d92db22dd9413c3fd18f2eb2972aa75c5195d5e1
Emmanuelahdamilola <emmanuelahdamilola1992@gmail.com>
2023-12-01 05:46:35 -0800

    script that sets up your web servers for the deployment of web_static

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,69 +1,38 @@
 #!/usr/bin/env bash
-# Bash script that sets up your web servers for the deployment of web_static
+# Script to set up the server file system for deployment
 
-# Ensure Nginx is installed
-if ! [ -x "$(command -v nginx)" ]; then
-  sudo apt-get update
-  sudo apt-get -y install nginx
-fi
+#!/usr/bin/env bash
+# Script to set up the server file system for deployment
 
-# Create /data/ directory if it doesn't exist
-if ! [ -d "/data/" ]; then
-  sudo mkdir /data/
-fi
+# Install Nginx
+sudo apt-get -y update
+sudo apt-get -y install nginx
+sudo service nginx start
 
-# Create /data/web_static/ directory if it doesn't exist
-if ! [ -d "/data/web_static/" ]; then
-  sudo mkdir /data/web_static/
-fi
+# Configure the file system
+sudo mkdir -p /data/web_static/shared/
+sudo mkdir -p /data/web_static/releases/test/
+# Configure the file system
+sudo mkdir -p /data/web_static/shared/
+sudo mkdir -p /data/web_static/releases/test/
 
-# Create /data/web_static/releases/ directory if it doesn't exist
-if ! [ -d "/data/web_static/releases/" ]; then
-  sudo mkdir /data/web_static/releases/
-fi
+# Create a fake HTML document
+echo "<!DOCTYPE html>
+<html lang=\"en\">
+<head>
+</head>
+<body>
+    <h1>Holberton School</h1>
+</body>
+</html>" | sudo tee /data/web_static/releases/test/index.html > /dev/null
 
-# Create /data/web_static/shared/ directory if it doesn't exist
-if ! [ -d "/data/web_static/shared/" ]; then
-  sudo mkdir /data/web_static/shared/
-fi
-
-# Create /data/web_static/releases/test/ directory if it doesn't exist
-if ! [ -d "/data/web_static/releases/test/" ]; then
-  sudo mkdir /data/web_static/releases/test/
-fi
-
-# Create a fake HTML file for testing Nginx configuration
-echo "<html><head></head><body>Holberton School</body></html>" > /data/web_static/releases/test/index.html
-
-# Create or recreate symbolic link /data/web_static/current
-#if [ -L "/data/web_static/current" ]; then
-#  sudo rm /data/web_static/current
-#fi
 sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
 
-# Set ownership of /data/ to the ubuntu user and group, recursively
+# Set permissions
 sudo chown -R ubuntu:ubuntu /data/
 
-# Update Nginx configuration to serve content from /data/web_static/current/ to /hbnb_static
-printf %s 'server {
-    listen 80 default_server;
-    listen [::]:80 default_server;
-    add_header X-Served-By $HOSTNAME;
-    root /var/www/html;
-    index index.html index.htm;
+# Configure Nginx
+sudo sed -i '44i \\n\tlocation /hbnb_static {\n\t\talias /data/web_static/current/;\n\t}' /etc/nginx/sites-available/default
 
-    location /hbnb_static {
-        alias /data/web_static/current/;
-	index index.html index.htm;
-    }
-
-    error_page 404 /404.html;
-    location /404 {
-    root /var/www/html;
-    internal;
-    }
-}' > /etc/nginx/sites-available/default
-
-# Restart Nginx to apply the changes
+# Restart the web server
 sudo service nginx restart
-
