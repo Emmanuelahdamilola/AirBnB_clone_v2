cfc2fadc24961e86653541716a1a1f332a88b294
Emmanuelahdamilola <emmanuelahdamilola1992@gmail.com>
2023-12-11 20:11:49 -0800

    script that starts a Flask web application

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -12,11 +12,13 @@ def hello_flask():
     """
     return 'Hello HBNB!'
 
+
 @app.route('/hbnb')
 def hello():
     """Return string when route queried
     """
-    return 'HBNB!'
+    return 'HBNB'
+
 
 if __name__ == '__main__':
     app.url_map.strict_slashes = False
