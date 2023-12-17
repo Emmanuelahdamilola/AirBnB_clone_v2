320e554836744e7312348fdb800406e3433f226d
Emmanuelahdamilola <emmanuelahdamilola1992@gmail.com>
2023-12-16 19:27:57 -0800

    Deploy archive!

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -5,17 +5,18 @@ from fabric.api import *
 from datetime import datetime
 from os import path
 
+
 # Define the server configuration
 env.hosts = ['18.210.33.153', '54.236.27.119']
 env.user = 'ubuntu'
 env.key_filename = '~/.ssh/school'
 
+
 def do_deploy(archive_path):
-    """Deploy web files to server
-    """
+    """Deploy web files to server"""
     try:
         # Check if the archive_path exists
-        if not (path.exists(archive_path)):
+        if not path.exists(archive_path):
             return False
 
         # Upload the archive to the /tmp/ directory on the server
@@ -24,30 +25,31 @@ def do_deploy(archive_path):
         # Create the target directory with a timestamp
         file_name = archive_path.split('/')[-1]
         time_stamp = file_name.split('.')[0]
-        target_directory = '/data/web_static/releases/web_static_{}/'.format(time_stamp)
-        run('sudo mkdir -p {}'.format(target_directory))
+        target_dir = f'/data/web_static/releases/web_static_{time_stamp}/'
+        run('sudo mkdir -p {}'.format(target_dir))
 
         # Uncompress the archive and delete the .tgz file
-        run('sudo tar -xzf /tmp/{} -C {}'.format(file_name, target_directory))
+        run('sudo tar -xzf /tmp/{} -C {}'.format(file_name, target_dir))
 
         # Remove the uploaded archive
         run('sudo rm /tmp/{}'.format(file_name))
 
         # Move contents into the host web_static directory
-        run('sudo mv {}/web_static/* {}/'.format(target_directory, target_directory))
+        run('sudo mv {}/web_static/* {}/'.format(target_dir, target_dir))
 
         # Remove the extraneous web_static directory
-        run('sudo rm -rf {}/web_static'.format(target_directory))
+        run('sudo rm -rf {}/web_static'.format(target_dir))
 
         # Delete pre-existing symbolic link
         run('sudo rm -rf /data/web_static/current')
 
         # Re-establish symbolic link to the new deployment
-        run('sudo ln -s {} /data/web_static/current'.format(target_directory))
-    except:
+        run('sudo ln -s {} /data/web_static/current'.format(target_dir))
+    except Exception as e:
+        # Print the exception for debugging purposes
+        print(e)
         # Return False on failure
         return False
 
     # Return True on success
     return True
-
