f27abaa7df19e1dfa1a24b7c5c21e96cbbcb41ff
Emmanuelahdamilola <emmanuelahdamilola1992@gmail.com>
2023-12-16 19:59:26 -0800

    Deploy archive!

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -6,7 +6,6 @@ from datetime import datetime
 from os import path
 
 
-# Define the server configuration
 env.hosts = ['18.210.33.153', '54.236.27.119']
 env.user = 'ubuntu'
 env.key_filename = '~/.ssh/school'
@@ -26,25 +25,25 @@ def do_deploy(archive_path):
         file_name = archive_path.split('/')[-1]
         time_stamp = file_name.split('.')[0]
         target_dir = f'/data/web_static/releases/web_static_{time_stamp}/'
-        run('sudo mkdir -p {}'.format(target_dir))
+        run(f'sudo mkdir -p {target_dir}')
 
         # Uncompress the archive and delete the .tgz file
-        run('sudo tar -xzf /tmp/{} -C {}'.format(file_name, target_dir))
+        run(f'sudo tar -xzf /tmp/{file_name} -C {target_dir}')
 
         # Remove the uploaded archive
-        run('sudo rm /tmp/{}'.format(file_name))
+        run(f'sudo rm /tmp/{file_name}')
 
         # Move contents into the host web_static directory
-        run('sudo mv {}/web_static/* {}/'.format(target_dir, target_dir))
+        run(f'sudo mv {target_dir}/web_static/* {target_dir}/')
 
         # Remove the extraneous web_static directory
-        run('sudo rm -rf {}/web_static'.format(target_dir))
+        run(f'sudo rm -rf {target_dir}/web_static')
 
         # Delete pre-existing symbolic link
         run('sudo rm -rf /data/web_static/current')
 
         # Re-establish symbolic link to the new deployment
-        run('sudo ln -s {} /data/web_static/current'.format(target_dir))
+        run(f'sudo ln -s {target_dir} /data/web_static/current')
     except Exception as e:
         # Print the exception for debugging purposes
         print(e)
